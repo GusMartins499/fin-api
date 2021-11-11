f08e98578b9b314f07d863677595725688aeecf0
gusmartins499 <gustavosm994@gmail.com>
2021-11-11 13:19:49 +0000

    create account

--- src/server.js
+++ src/server.js
@@ -1,5 +1,24 @@
 import express from "express";
+import { v4 } from "uuid";
 
 const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+  const uuidv4 = v4();
+
+  customers.push({
+    uuidv4,
+    cpf,
+    name,
+    statement: [],
+  });
+
+  return response.status(201).send();
+});
 
 app.listen(3333, () => console.log("🚀 Server started at port 3333"));
