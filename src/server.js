ea43b86303b74f06dc43153ae06ba766f92fd2ec
gusmartins499 <gustavosm994@gmail.com>
2021-11-11 14:46:56 +0000

    get statement by date

--- src/server.js
+++ src/server.js
@@ -60,6 +60,19 @@ app.get("/statement", (request, response) => {
   return response.json(customer.statement);
 });
 
+app.get("/statement/date", (request, response) => {
+  const { customer } = request;
+  const { date } = request.query;
+  const dateFormat = new Date(date + " 00:00");
+
+  const statement = customer.statement.filter(
+    (statement) =>
+      statement.created_at.toDateString() ===
+      new Date(dateFormat).toDateString()
+  );
+  return response.json(statement);
+});
+
 app.post("/deposit", (request, response) => {
   const { description, amount } = request.body;
   const { customer } = request;
